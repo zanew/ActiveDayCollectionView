7cf23c18d9deeaa9b1622ff6948acac79a0381ae
Zane Whitney <zane.whitney@gmail.com>
2020-04-16 14:45:35 -0400

    Add special compiler flag just for me.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DayOfWeekCollectionView",
-            dependencies: []),
+            dependencies: [],
+            cSettings: [
+                .define("FULL_WEEKDAY_PICKER"),
+            ]),
         .testTarget(
             name: "DayOfWeekCollectionViewTests",
             dependencies: ["DayOfWeekCollectionView"]),
